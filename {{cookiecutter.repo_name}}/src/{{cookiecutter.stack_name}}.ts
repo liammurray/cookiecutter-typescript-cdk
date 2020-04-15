63c50fcf9aab420a3c7f989760f1150f96d52410
Liam Murray <liammurray@users.noreply.github.com>
2020-04-15 12:06:04 -0700

    Tweaks

--- {{cookiecutter.repo_name}}/src/{{cookiecutter.stack_name}}.ts
+++ {{cookiecutter.repo_name}}/src/{{cookiecutter.stack_name}}.ts
@@ -10,6 +10,5 @@ export interface {{cookiecutter.stack_name}}Props extends cdk.StackProps {
 export default class {{cookiecutter.stack_name}} extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: {{cookiecutter.stack_name}}Props) {
     super(scope, id, props)
-
   }
 }
--- {{cookiecutter.repo_name}}/test/{{cookiecutter.stack_name}}.test.ts
+++ {{cookiecutter.repo_name}}/test/{{cookiecutter.stack_name}}.test.ts
@@ -2,7 +2,6 @@ import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert'
 import * as cdk from '@aws-cdk/core'
 import {{cookiecutter.stack_name}} from '../src/{{cookiecutter.stack_name}}'
 
-
 test('Empty Stack', () => {
   const app = new cdk.App()
   const stack = new {{cookiecutter.stack_name}}(app, 'test-stack', {})
