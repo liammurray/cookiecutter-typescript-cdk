31acb4e4b2c0d2295a442e0c4421dd25a954b139
Liam Murray <liammurray@users.noreply.github.com>
2020-06-23 16:38:53 -0700

    Comment out stuff that produces lint error

--- {{cookiecutter.repo_name}}/src/{{cookiecutter.stack_name}}.ts
+++ {{cookiecutter.repo_name}}/src/{{cookiecutter.stack_name}}.ts
@@ -1,10 +1,10 @@
 import * as cdk from '@aws-cdk/core'
 
-export interface {{cookiecutter.stack_name}}Props extends cdk.StackProps {
-}
+// export interface {{cookiecutter.stack_name}}Props extends cdk.StackProps {
+// }
 
 export default class {{cookiecutter.stack_name}} extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: {{cookiecutter.stack_name}}Props) {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props)
   }
 }
